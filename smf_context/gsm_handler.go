890e712df00c1389cc51d8ffb9e0e5be50184c70
Fu Lian Weng <turtle11311@gmail.com>
2020-02-13 18:30:28 +0800

    Add handle pdu establishment accept pdu type

--- smf_context/gsm_handler.go
+++ smf_context/gsm_handler.go
@@ -4,5 +4,18 @@ import (
 	"gofree5gc/lib/nas/nasMessage"
 )
 
+func (smContext *SMContext) HandlePDUSessionEstablishmentRequest(req *nasMessage.PDUSessionEstablishmentRequest) {
+	// Retrieve PDUSessionID
+	smContext.PDUSessionID = int32(req.PDUSessionID.GetPDUSessionID())
+
+	// Handle PDUSessionType
+	if req.PDUSessionType != nil {
+		smContext.SelectedPDUSessionType = req.PDUSessionType.GetPDUSessionTypeValue()
+	} else {
+		// Default to IPv4
+		smContext.SelectedPDUSessionType = nasMessage.PDUSessionTypeIPv4
+	}
+}
+
 func (smContext *SMContext) HandlePDUSessionReleaseRequest(req *nasMessage.PDUSessionReleaseRequest) {
 }
