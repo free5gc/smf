0408d0b47e8947c8b212790d9cab0f16f4957cb6
danielh1204 <80144195+danielh1204@users.noreply.github.com>
2023-08-16 22:50:34 +0800

    compare snssai with SnssaiEqualFold (#82)
    * change sd to lowercase
    
    gofumpt-ed
    
    deleted if
    
    use EqualFold
    
    changed sd to lowercase (#81)
    
    * change sd to lowercase
    
    * gofumpt-ed
    
    * deleted if
    
    Revert "changed sd to lowercase (#81)" (#83)
    
    This reverts commit abec5eee22d00ed563f492357ecf680449aead69.
    
    * update

--- internal/context/context.go
+++ internal/context/context.go
@@ -91,7 +91,7 @@ func (s *SMFContext) ListenIP() net.IP {
 // RetrieveDnnInformation gets the corresponding dnn info from S-NSSAI and DNN
 func RetrieveDnnInformation(Snssai *models.Snssai, dnn string) *SnssaiSmfDnnInfo {
 	for _, snssaiInfo := range GetSelf().SnssaiInfos {
-		if snssaiInfo.Snssai.Sst == Snssai.Sst && snssaiInfo.Snssai.Sd == Snssai.Sd {
+		if snssaiInfo.Snssai.EqualModelsSnssai(Snssai) {
 			return snssaiInfo.DnnInfos[dnn]
 		}
 	}
