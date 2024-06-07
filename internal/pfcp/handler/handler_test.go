44ed80c5541b85738f25f3fa44f793f08b30a4ea
ming-hsien <w2bc8860@gmail.com>
2024-06-07 17:02:22 +0000

    feat: test file

--- internal/pfcp/handler/handler_test.go
+++ internal/pfcp/handler/handler_test.go
@@ -1,11 +1,46 @@
 package handler_test
 
 import (
+	// "net"
 	"testing"
+
+	// "github.com/free5gc/pfcp"
+	// "github.com/free5gc/pfcp/pfcpType"
+	// "github.com/free5gc/pfcp/pfcpUdp"
+	// "github.com/free5gc/smf/internal/logger"
+	// "github.com/free5gc/smf/internal/pfcp/handler"
+	// "github.com/stretchr/testify/assert"
 )
 
 func TestHandlePfcpAssociationSetupRequest(t *testing.T) {
+	// remoteAddr := &net.UDPAddr{
+	// 	IP:   net.ParseIP("192.168.1.1"),
+	// 	Port: 12345,
+	// }
+
+	// testPfcpReq := &pfcp.Message{
+	// 	Header: pfcp.Header{
+	// 		Version:         1,
+	// 		MP:              0,
+	// 		S:               0,
+	// 		MessageType:     pfcp.PFCP_ASSOCIATION_SETUP_REQUEST,
+	// 		MessageLength:   9,
+	// 		SEID:            0,
+	// 		SequenceNumber:  1,
+	// 		MessagePriority: 0,
+	// 	},
+	// 	Body: pfcp.PFCPAssociationSetupRequest{
+	// 		NodeID: &pfcpType.NodeID{
+	// 			NodeIdType: 0,
+	// 			IP:         net.ParseIP("192.168.1.1").To4(),
+	// 		},
+	// 	},
+	// }
+	// msg := pfcpUdp.NewMessage(remoteAddr, testPfcpReq)
+	// mockLogger := &logger.MockLogger{}
+	// handler.HandlePfcpAssociationSetupRequest(msg)
+	// assert.Equal(t, true, mockLogger.ErrorfInvoked)
 }
 
-func TestHandlePfcpAssociationReleaseRequest(t *testing.T) {
-}
+// func TestHandlePfcpAssociationReleaseRequest(t *testing.T) {
+// }
--- internal/pfcp/message/build_test.go
+++ internal/pfcp/message/build_test.go
@@ -4,12 +4,89 @@ import (
 	"net"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
+	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/pfcp"
+	"github.com/free5gc/pfcp/pfcpType"
+	"github.com/free5gc/smf/internal/context"
 	"github.com/free5gc/smf/internal/pfcp/message"
 	"github.com/free5gc/smf/internal/pfcp/udp"
-	"github.com/free5gc/pfcp/pfcpType"
+	"github.com/free5gc/smf/pkg/factory"
+	"github.com/stretchr/testify/assert"
 )
+
+var userPlaneConfig = factory.UserPlaneInformation{
+	UPNodes: map[string]*factory.UPNode{
+		"GNodeB": {
+			Type: "AN",
+		},
+		"UPF1": {
+			Type:   "UPF",
+			NodeID: "10.4.0.11",
+			Addr:   "10.4.0.11",
+			SNssaiInfos: []*factory.SnssaiUpfInfoItem{
+				{
+					SNssai: &models.Snssai{
+						Sst: 1,
+						Sd:  "010203",
+					},
+					DnnUpfInfoList: []*factory.DnnUpfInfoItem{
+						{
+							Dnn:      "internet",
+							DnaiList: []string{"mec"},
+						},
+					},
+				},
+			},
+			InterfaceUpfInfoList: []*factory.InterfaceUpfInfoItem{
+				{
+					InterfaceType: "N3",
+					Endpoints: []string{
+						"10.3.0.11",
+					},
+					NetworkInstances: []string{"internet"},
+				},
+				{
+					InterfaceType: "N9",
+					Endpoints: []string{
+						"10.3.0.11",
+					},
+					NetworkInstances: []string{"internet"},
+				},
+			},
+		},
+	},
+	Links: []*factory.UPLink{
+		{
+			A: "GNodeB",
+			B: "UPF1",
+		},
+		{
+			A: "UPF1",
+			B: "UPF2",
+		},
+	},
+}
+
+var testConfig = factory.Config{
+	Info: &factory.Info{
+		Version:     "1.0.0",
+		Description: "SMF procdeure test configuration",
+	},
+	Configuration: &factory.Configuration{
+		Sbi: &factory.Sbi{
+			Scheme:       "http",
+			RegisterIPv4: "127.0.0.1",
+			BindingIPv4:  "127.0.0.1",
+			Port:         8000,
+		},
+		UserPlaneInformation: userPlaneConfig,
+	},
+}
+
+func initConfig() {
+	context.InitSmfContext(&testConfig)
+	factory.SmfConfig = &testConfig
+}
 
 func TestBuildPfcpAssociationSetupRequest(t *testing.T) {
 	emptyReq, err := message.BuildPfcpAssociationSetupRequest()
@@ -22,13 +99,13 @@ func TestBuildPfcpAssociationSetupRequest(t *testing.T) {
 	assert.Equal(t, net.IP(nil), emptyReq.NodeID.IP)
 	assert.Equal(t, "", emptyReq.NodeID.FQDN)
 
-	assert.Equal(t, 
-		udp.ServerStartTime, 
+	assert.Equal(t,
+		udp.ServerStartTime,
 		emptyReq.RecoveryTimeStamp.RecoveryTimeStamp)
 	assert.Nil(t,
 		emptyReq.UPFunctionFeatures)
-	assert.Equal(t, 
-		pfcpType.CPFunctionFeatures{SupportedFeatures: 0}, 
+	assert.Equal(t,
+		pfcpType.CPFunctionFeatures{SupportedFeatures: 0},
 		*emptyReq.CPFunctionFeatures)
 }
 
@@ -45,8 +122,8 @@ func TestBuildPfcpAssociationSetupResponse(t *testing.T) {
 
 	assert.Nil(t,
 		rsp.UPFunctionFeatures)
-	assert.Equal(t, 
-		pfcpType.CPFunctionFeatures{SupportedFeatures: 0}, 
+	assert.Equal(t,
+		pfcpType.CPFunctionFeatures{SupportedFeatures: 0},
 		*rsp.CPFunctionFeatures)
 }
 
@@ -72,11 +149,42 @@ func TestBuildPfcpAssociationReleaseResponse(t *testing.T) {
 }
 
 func TestBuildPfcpSessionEstablishmentRequest(t *testing.T) {
-	
+	initConfig()
+	var NodeID = pfcpType.NodeID{
+		NodeIdType: 0,
+		IP:         net.ParseIP("10.4.0.11").To4(),
+	}
+	smctx := context.NewSMContext("imsi-208930000000001", 10)
+	pdrList := make([]*context.PDR, 0, 2)
+	farList := make([]*context.FAR, 0, 2)
+	qerList := make([]*context.QER, 0, 2)
+	urrList := make([]*context.URR, 0, 2)
+	barList := make([]*context.BAR, 0, 2)
+
+	smctx.PFCPContext["10.4.0.11"] = &context.PFCPSessionContext{}
+
+	req, err := message.BuildPfcpSessionEstablishmentRequest(NodeID, "10.4.0.11", smctx, pdrList, farList, barList, qerList, urrList)
+	if err != nil {
+		t.Errorf("TestBuildPfcpSessionEstablishmentRequest failed: %v", err)
+	}
+
+	createPDR := make([]*pfcp.CreatePDR, 0)
+	assert.Equal(t, uint8(0), req.NodeID.NodeIdType)
+	assert.NotNil(t, req.CPFSEID)
+	assert.NotNil(t, req.PDNType)
+	assert.Equal(t, createPDR, req.CreatePDR)
 }
 
+// hsien
 func TestBuildPfcpSessionEstablishmentResponse(t *testing.T) {
-	
+	rsp, err := message.BuildPfcpSessionEstablishmentResponse()
+	if err != nil {
+		t.Errorf("TestBuildPfcpSessionEstablishmentResponse failed: %v", err)
+	}
+	assert.Equal(t, uint8(0), rsp.NodeID.NodeIdType)
+	assert.Equal(t, pfcpType.CauseRequestAccepted, rsp.Cause.CauseValue)
+	assert.NotNil(t, rsp.UPFSEID)
+	assert.NotNil(t, rsp.CreatedPDR)
 }
 
 func TestBuildPfcpSessionModificationRequest(t *testing.T) {
@@ -84,16 +192,20 @@ func TestBuildPfcpSessionModificationRequest(t *testing.T) {
 }
 
 func TestBuildPfcpSessionModificationResponse(t *testing.T) {
-
+	rsp, err := message.BuildPfcpSessionEstablishmentResponse()
+	if err != nil {
+		t.Errorf("BuildPfcpSessionModificationResponse failed: %v", err)
+	}
+	assert.Equal(t, pfcpType.CauseRequestAccepted, rsp.Cause.CauseValue)
+	assert.NotNil(t, rsp.OffendingIE)
+	assert.NotNil(t, rsp.CreatedPDR)
 }
-
 
 func TestBuildPfcpSessionDeletionResponse(t *testing.T) {
 	_, err := message.BuildPfcpSessionDeletionResponse()
 	if err != nil {
 		t.Errorf("TestBuildPfcpSessionDeletionResponse failed: %v", err)
 	}
-
 }
 
 func TestBuildPfcpSessionReportResponse(t *testing.T) {
