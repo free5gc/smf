c2f2ec6363bbb0c03c2e468a837a2fd67dc3d3c1
ntut-xuan <t109590031@ntut.org.tw>
2024-08-07 07:25:04 +0000

    Feat: Add tests

--- internal/util/tests/qos_convert_test.go
+++ internal/util/tests/qos_convert_test.go
@@ -6,9 +6,77 @@ import (
 	"github.com/free5gc/smf/internal/util"
 )
 
-func TestBitRateToKbpsWithValidBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
+func TestBitRateToKbpsWithValidBpsBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
+	var bitrate string = "1000 bps"
+	var correctBitRateKbps uint64 = 1
+
+	bitrateKbps, err := util.BitRateTokbps(bitrate)
+
+	t.Log("Check: err should be nil since act should work correctly.")
+	if err != nil {
+		t.Errorf("Error: err should be nil but it returns %s", err)
+	}
+	t.Log("Check: convert should act correctly.")
+	if bitrateKbps != correctBitRateKbps {
+		t.Errorf("Error: bitrate convert failed. Expect: %d. Actually: %d", correctBitRateKbps, bitrateKbps)
+	}
+	t.Log("Passed.")
+}
+
+func TestBitRateToKbpsWithValidKbpsBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
+	var bitrate string = "1000 Kbps"
+	var correctBitRateKbps uint64 = 1000
+
+	bitrateKbps, err := util.BitRateTokbps(bitrate)
+
+	t.Log("Check: err should be nil since act should work correctly.")
+	if err != nil {
+		t.Errorf("Error: err should be nil but it returns %s", err)
+	}
+	t.Log("Check: convert should act correctly.")
+	if bitrateKbps != correctBitRateKbps {
+		t.Errorf("Error: bitrate convert failed. Expect: %d. Actually: %d", correctBitRateKbps, bitrateKbps)
+	}
+	t.Log("Passed.")
+}
+
+func TestBitRateToKbpsWithValidMbpsBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
 	var bitrate string = "1000 Mbps"
 	var correctBitRateKbps uint64 = 1000000
+
+	bitrateKbps, err := util.BitRateTokbps(bitrate)
+
+	t.Log("Check: err should be nil since act should work correctly.")
+	if err != nil {
+		t.Errorf("Error: err should be nil but it returns %s", err)
+	}
+	t.Log("Check: convert should act correctly.")
+	if bitrateKbps != correctBitRateKbps {
+		t.Errorf("Error: bitrate convert failed. Expect: %d. Actually: %d", correctBitRateKbps, bitrateKbps)
+	}
+	t.Log("Passed.")
+}
+
+func TestBitRateToKbpsWithValidGbpsBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
+	var bitrate string = "1000 Gbps"
+	var correctBitRateKbps uint64 = 1000000000
+
+	bitrateKbps, err := util.BitRateTokbps(bitrate)
+
+	t.Log("Check: err should be nil since act should work correctly.")
+	if err != nil {
+		t.Errorf("Error: err should be nil but it returns %s", err)
+	}
+	t.Log("Check: convert should act correctly.")
+	if bitrateKbps != correctBitRateKbps {
+		t.Errorf("Error: bitrate convert failed. Expect: %d. Actually: %d", correctBitRateKbps, bitrateKbps)
+	}
+	t.Log("Passed.")
+}
+
+func TestBitRateToKbpsWithValidTbpsBitRateShouldReturnValidKbpsBitRate(t *testing.T) {
+	var bitrate string = "1000 Tbps"
+	var correctBitRateKbps uint64 = 1000000000000
 
 	bitrateKbps, err := util.BitRateTokbps(bitrate)
 
