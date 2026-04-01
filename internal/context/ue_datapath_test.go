ee12ec97fba149224124f9ca71eaf05a92eafd92
DBGR18 <dbgr1218@gmail.com>
2026-04-01 14:24:41 +0800

    fix: rename returned error variable in test

--- internal/context/ue_datapath_test.go
+++ internal/context/ue_datapath_test.go
@@ -152,8 +152,8 @@ func TestNewUEPreConfigPaths(t *testing.T) {
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			retUePreConfigPaths, err := context.NewUEPreConfigPaths(tc.inPaths)
-			require.Nil(t, err)
+			retUePreConfigPaths, preConfigErr := context.NewUEPreConfigPaths(tc.inPaths)
+			require.Nil(t, preConfigErr)
 			require.NotNil(t, retUePreConfigPaths.PathIDGenerator)
 			for pathIndex, path := range tc.inPaths {
 				retDataPath := retUePreConfigPaths.DataPathPool[int64(pathIndex+1)]
